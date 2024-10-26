6b9e4ded6be869b1c09bc6e0aca5fb1f23c62803
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-10-26 13:18:08 +0600

    Added Material UI to my project. Installed Roboto links in my projects head tag. Also installed the Material Icons font. Created Navbar component. In my navbar included Navlinks to Quotes page and Submit new quote page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
-const App = () => <>hello</>;
+import Navbar from './components/Navbar/Navbar.tsx';
+import { Container, Typography } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Quotes from './containers/Quotes/Quotes.tsx';
+import QuoteForm from './containers/QuoteForm/QuoteForm.tsx';
+
+const App = () => (
+  <>
+    <header>
+      <Navbar />
+    </header>
+    <Container maxWidth="lg">
+      <Routes>
+        <Route path="/quotes" element={<Quotes/>}></Route>
+        <Route path="/submit-new-quote" element={<QuoteForm/>}></Route>
+        <Route path="*" element={<Typography variant="h6">Page not found</Typography>}></Route>
+      </Routes>
+    </Container>
+
+  </>
+);
 
 export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1, mb: 5 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{mr: 2}}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <Typography color="inherit" to="/" variant="h6" component={NavLink} sx={{flexGrow: 1, textDecoration: "none"}}>
+            Quotes Central
+          </Typography>
+          <Typography>
+            <Typography color="inherit" to="/quotes" variant="h7" component={NavLink} sx={{flexGrow: 1, textDecoration: "none", m: 3}}>
+              Quotes
+            </Typography>
+            <Typography color="inherit" to="/submit-new-quote" variant="h7" component={NavLink} sx={{flexGrow: 1, textDecoration: "none"}}>
+              Submit new quote
+            </Typography>
+          </Typography>
+          <IconButton size="large" aria-label="search" color="inherit">
+            <SearchIcon/>
+          </IconButton>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
